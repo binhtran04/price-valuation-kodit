032cb1770c06f8bc9b279c298b8825488b422d59
Binh Tran <thanhbinh.tran93@gmail.com>
2019-01-20 14:49:24 +0200

    Updates

---
+++ precache-manifest.9074fa70865ed0d080d587efb83295e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9933c873aa9ee9483eeb",
+    "url": "/price-valuation-kodit/static/js/runtime~main.9933c873.js"
+  },
+  {
+    "revision": "34a8eed693f479597e47",
+    "url": "/price-valuation-kodit/static/js/main.34a8eed6.chunk.js"
+  },
+  {
+    "revision": "48ad99134a31e051a7ae",
+    "url": "/price-valuation-kodit/static/js/1.48ad9913.chunk.js"
+  },
+  {
+    "revision": "34a8eed693f479597e47",
+    "url": "/price-valuation-kodit/static/css/main.65bfc69c.chunk.css"
+  },
+  {
+    "revision": "cead0a91d7594b2fb5da51e7904e5747",
+    "url": "/price-valuation-kodit/index.html"
+  }
+];
